9ea7071bcef31c91b7b2e7fb88eca7c89586bc77
dvinitsk <dvinitsk@my.yorku.ca>
2024-08-23 09:07:04 -0400

    Added update method that controls rotation with a and d keys

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def main():
                 return
         screen.fill((0,0,0))
         player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000 #returns the delta time
 
