d692c3330ac81092e3eb82df7405ca00756503c7
dvinitsk <dvinitsk@my.yorku.ca>
2024-08-25 17:29:19 -0400

    Added 2 groups: updatable and drawable, now the draw and update methods are run on all objects that are part of these groups. Also added the asteroid class

--- main.py
+++ main.py
@@ -11,16 +11,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group() #all objects that can be updated
+    drawable = pygame.sprite.Group() #all objects that can be drawn
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        screen.fill((0,0,0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
         
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+    
         pygame.display.flip()
         dt = clock.tick(60) / 1000 #returns the delta time
 
